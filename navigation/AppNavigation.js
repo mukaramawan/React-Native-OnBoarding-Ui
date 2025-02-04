3479a13f9735e64e988146bdb19b338d7e95451b
Mukaram Awan <141765944+mukaramawan@users.noreply.github.com>
2025-02-04 11:02:33 +0500

    Implement Stack Navigation & Install packages
    Install native-stack & react-native-onboarding-swiper

---
+++ navigation/AppNavigation.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import OnBoardingScreen from "../screens/OnBoardingScreen";
+import HomeScreen from "../screens/HomeScreen";
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Onboarding">
+        <Stack.Screen
+          name="Onboarding"
+          options={{ headerShown: false }}
+          component={OnBoardingScreen}
+        />
+        <Stack.Screen
+          name="Home"
+          options={{ headerShown: false }}
+          component={HomeScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigation;
---
+++ screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const HomeScreen = () => {
+  return <SafeAreaView></SafeAreaView>;
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
