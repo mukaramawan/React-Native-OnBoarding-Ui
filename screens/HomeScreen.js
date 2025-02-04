67749e235ff879b4cda529a194ae9fa04673fc04
Mukaram Awan <141765944+mukaramawan@users.noreply.github.com>
2025-02-04 12:51:41 +0500

    OnBoarding with Lottie Animations
    Install lottie-react-native to add animations

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,35 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Dimensions, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import LottieView from "lottie-react-native";
+
+const { width, height } = Dimensions.get("window");
 
 const HomeScreen = () => {
-  return <SafeAreaView></SafeAreaView>;
+  return (
+    <>
+      <SafeAreaView style={styles.container}>
+        <LottieView
+          style={styles.lottie}
+          source={require("../assets/Animation - 1738654585887.json")}
+          autoPlay
+          loop
+          
+        />
+        <Text>Welcome Home!</Text>
+      </SafeAreaView>
+    </>
+  );
 };
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  lottie: {
+    height: width * 0.9,
+    width: width,
+  },
+});
--- screens/OnBoardingScreen.js
+++ screens/OnBoardingScreen.js
@@ -1,10 +1,89 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
+import Onboarding from "react-native-onboarding-swiper";
+import LottieView from "lottie-react-native";
+import { useNavigation } from "@react-navigation/native";
+
+const { width, height } = Dimensions.get("window");
 
 const OnBoardingScreen = () => {
+  const navigation = useNavigation();
+
+  const handleDone = () => {
+    navigation.navigate("Home");
+  };
+
+  const doneButton = ({ ...props }) => {
+    return (
+      <TouchableOpacity style={styles.doneButton} {...props}>
+        <Text>Done</Text>
+      </TouchableOpacity>
+    );
+  };
   return (
     <View style={styles.container}>
-      <Text>OnBoardingScreen</Text>
+      <Onboarding
+        onDone={handleDone}
+        onSkip={handleDone}
+        // DoneButtonComponent={doneButton} //Can pass custom Component
+        containerStyles={{ paddingHorizontal: 15 }}
+        pages={[
+          {
+            backgroundColor: "#a7f3d0",
+            image: (
+              <View>
+                <LottieView
+                  style={styles.lottie}
+                  source={require("../assets/exam-prep1.json")}
+                  autoPlay
+                  loop
+                />
+              </View>
+            ),
+            title: "Learn Anytime, Anywhere",
+            subtitle:
+              "Access courses offline or online, at your convenience. Learn without limits.",
+          },
+          {
+            backgroundColor: "#f3f3c7",
+            image: (
+              <View>
+                <LottieView
+                  style={styles.lottie}
+                  source={require("../assets/exam-prep1.json")}
+                  autoPlay
+                  loop
+                />
+              </View>
+            ),
+            title: "Master Your Exams",
+            subtitle:
+              "Access comprehensive study materials, structured courses, and past papers to build a strong foundation.",
+          },
+          {
+            backgroundColor: "#a78bfa",
+            image: (
+              <View>
+                <LottieView
+                  style={styles.lottie}
+                  source={require("../assets/exam-prep1.json")}
+                  autoPlay
+                  loop
+                />
+              </View>
+            ),
+            title: "Unlock Opportunities",
+            subtitle:
+              "Get real-time Educational News, and never miss critical updates for your academic journey.",
+          },
+        ]}
+      />
     </View>
   );
 };
@@ -16,4 +95,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "white",
   },
+  lottie: {
+    height: width * 0.9,
+    width: width,
+  },
+  doneButton: {},
 });
